663fc6ee1b48f01c961991c0fc8bac87a890cd4d
Masashi Shinbara <shin1x1@gmail.com>
2015-12-16 04:37:20 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/FooController.php
+++ app/Http/Controllers/FooController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Shin1x1\Book\Domain\BookRepository;
 
-class FooController extends Controller {
-public function books(BookRepository $repository)
+class FooController extends Controller
 {
-return $repository->resolveAll();
-}
+    public function books(BookRepository $repository)
+    {
+        return $repository->resolveAll();
+    }
 }
